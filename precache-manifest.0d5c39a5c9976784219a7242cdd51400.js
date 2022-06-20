5f70692dc80781e394d34a6a6dff9c03dbae9afe
ShavelD <legrand.97@mail.ru>
2022-06-20 12:28:18 +0300

    Updates

---
+++ precache-manifest.0d5c39a5c9976784219a7242cdd51400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050500ed217619e58f0303da70c46a3b",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "60c796e53a37bf815f0b",
+    "url": "/TodoList/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "e8c430a67ffe03f0f5fd",
+    "url": "/TodoList/static/js/2.baefa490.chunk.js"
+  },
+  {
+    "revision": "2550e5afe6e93adbeb7b915405b0a838",
+    "url": "/TodoList/static/js/2.baefa490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c796e53a37bf815f0b",
+    "url": "/TodoList/static/js/main.46b93b5f.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
